1282bd278a5bf1f112ac2f81acd5cda998169719
xeusteerapat <xeus085@gmail.com>
2024-01-01 15:50:29 +0700

    :sparkles: [feat] validation with sequence option

--- src/functions.ts
+++ src/functions.ts
@@ -1,13 +1,21 @@
-import * as E from 'fp-ts/lib/Either';
-import {
-  FieldNotEmpty,
-  ValidateAge,
-  ValidateGender,
-  UserRegistrationDto,
-} from './domain';
-import { pipe } from 'fp-ts/lib/function';
-import { NonEmptyArray, getSemigroup } from 'fp-ts/lib/NonEmptyArray';
 import { sequenceT } from 'fp-ts/lib/Apply';
+import * as E from 'fp-ts/lib/Either';
+import { NonEmptyArray, getSemigroup } from 'fp-ts/lib/NonEmptyArray';
+import { pipe } from 'fp-ts/lib/function';
+import {
+  CreateUser,
+  Europe,
+  FindGender,
+  FindRegion,
+  NorthAmerica,
+  Other,
+  Region,
+  UserRegistrationDto,
+  firstNameIso,
+  lastNameIso,
+} from './domain';
+import * as O from 'fp-ts/lib/Option';
+import { prismPositiveInteger } from 'newtype-ts/lib/PositiveInteger';
 
 const applicativeValidation = E.getApplicativeValidation(
   getSemigroup<string>()
@@ -88,3 +96,89 @@ const resultWithSequence = pipe(
   E.map(([evt]) => evt)
 );
 console.log(resultWithSequence); // { _tag: 'Left', left: [ 'Invalid age of 13', 'Invalid sex of G' ] }
+
+const america: NorthAmerica = { _type: 'NorthAmerica' };
+const europe: Europe = { _type: 'Europe' };
+const other: Other = { _type: 'Other' };
+
+const countryMappings: Record<string, Region> = {
+  Belgium: europe,
+  USA: america,
+  Germany: europe,
+  Thailand: other,
+};
+
+const createUser: CreateUser = (firstName, lastName, age, gender, region) => ({
+  firstName,
+  lastName,
+  age,
+  gender,
+  region,
+});
+
+const findRegion: FindRegion = country =>
+  countryMappings[country] ? O.some(countryMappings[country]) : O.none;
+
+const findGender: FindGender = sex =>
+  sex === 'M' || sex === 'F' || sex === 'X' ? O.some(sex) : O.none;
+
+const sequenceForOption = sequenceT(O.option);
+
+const newUserRegistrationEvent: UserRegistrationDto = {
+  firstName: 'Teerapat',
+  lastName: 'Xeus',
+  age: 37,
+  sex: 'M',
+  country: 'Thailand',
+};
+
+const newUser = pipe(
+  newUserRegistrationEvent,
+  evt =>
+    sequenceForOption(
+      O.some(firstNameIso.wrap(evt.firstName)),
+      O.some(lastNameIso.wrap(evt.lastName)),
+      prismPositiveInteger.getOption(evt.age),
+      findGender(evt.sex),
+      findRegion(evt.country)
+    ),
+  O.map(([f, l, a, g, c]) => createUser(f, l, a, g, c))
+);
+
+console.log(newUser);
+/**
+{
+  _tag: 'Some',
+  value: {
+    firstName: 'Teerapat',
+    lastName: 'Xeus',
+    age: 37,
+    gender: 'M',
+    region: { _type: 'Other' }
+  }
+}
+*/
+
+// example for failed validation
+const failedUserRegistrationEvent: UserRegistrationDto = {
+  firstName: 'Jane',
+  lastName: 'Doe',
+  age: 37,
+  sex: 'M',
+  country: 'Canada',
+};
+
+const failedValidateUser = pipe(
+  failedUserRegistrationEvent,
+  evt =>
+    sequenceForOption(
+      O.some(firstNameIso.wrap(evt.firstName)),
+      O.some(lastNameIso.wrap(evt.lastName)),
+      prismPositiveInteger.getOption(evt.age),
+      findGender(evt.sex),
+      findRegion(evt.country)
+    ),
+  O.map(([f, l, a, g, c]) => createUser(f, l, a, g, c))
+);
+
+console.log(failedValidateUser); // { _tag: 'None' }
